b45b8659efca5e87bca46af9429fed1a954ff6f7
RickyReyes <77644172+RickyReyes@users.noreply.github.com>
2022-10-05 11:05:05 -0700

    detail page done

--- src/components/DetailMain.jsx
+++ src/components/DetailMain.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+const DetailMain = ({ job }) => {
+	const {
+		postedAt,
+		contract,
+		theme,
+		position,
+		description,
+		location,
+		requirements,
+		role,
+	} = job;
+	return (
+		<main className="detail-main">
+			<div className="card__flex">
+				<p>{postedAt}</p>
+				<p>&#8226;</p>
+				<p>{contract}</p>
+			</div>
 
-const DetailMain = () => {
-	return <div>DetailMain</div>;
+			<h2>{position}</h2>
+
+			<small>{location}</small>
+			<button className="btn">Apply Now</button>
+			<p>{description}</p>
+			<h2 className="requirements">Requirements</h2>
+			<p>{requirements.content}</p>
+			<ul>
+				{requirements.items.map((item) => (
+					<li>
+						<span className="bullet">&#8226;</span>{" "}
+						<span className="item">{item}</span>
+					</li>
+				))}
+			</ul>
+			<h2>What You Will Do</h2>
+			<p>{role.content}</p>
+			<ol>
+				{role.items.map((item, id) => (
+					<li>
+						<span className="number">{id + 1}</span>
+						<div className="item">{item}</div>
+					</li>
+				))}
+			</ol>
+		</main>
+	);
 };
 
 export default DetailMain;
